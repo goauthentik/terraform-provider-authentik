6e997d7d66150de7dc9d86a5f1a29fdfca3ed29d
Jens Langhammer <jens.langhammer@beryju.org>
2021-07-17 18:16:10 +0200

    prepare initial release

--- main.go
+++ main.go
@@ -7,6 +7,15 @@ import (
 	"github.com/goauthentik/terraform-provider-authentik/authentik"
 )
 
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
+
+	// goreleaser can also pass the specific commit if you want
+	commit string = ""
+)
+
 //go:generate terraform fmt -recursive ./examples/
 
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
