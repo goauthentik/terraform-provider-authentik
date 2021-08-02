0aceb13520c5bc20698082bc8425585d42474635
Jens Langhammer <jens.langhammer@beryju.org>
2021-08-02 13:19:39 +0200

    add authentik_flow

---
+++ internal/provider/resource_flow.go
@@ -0,0 +1,124 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/goauthentik/terraform-provider-authentik/api"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceFlow() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceFlowCreate,
+		ReadContext:   resourceFlowRead,
+		UpdateContext: resourceFlowUpdate,
+		DeleteContext: resourceFlowDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"slug": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"title": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"designation": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"policy_engine_mode": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  api.POLICYENGINEMODE_ANY,
+			},
+			"compatibility_mode": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+		},
+	}
+}
+
+func resourceFlowSchemaToModel(d *schema.ResourceData, c *APIClient) (*api.FlowRequest, diag.Diagnostics) {
+	m := api.FlowRequest{
+		Name:              d.Get("name").(string),
+		Slug:              d.Get("slug").(string),
+		Title:             d.Get("title").(string),
+		Designation:       api.FlowDesignationEnum(d.Get("designation").(string)),
+		CompatibilityMode: boolToPointer(d.Get("compatibility_mode").(bool)),
+	}
+
+	pm := api.PolicyEngineMode(d.Get("policy_engine_mode").(string))
+	m.PolicyEngineMode = &pm
+	return &m, nil
+}
+
+func resourceFlowCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*APIClient)
+
+	app, diags := resourceFlowSchemaToModel(d, c)
+	if diags != nil {
+		return diags
+	}
+
+	res, hr, err := c.client.FlowsApi.FlowsInstancesCreate(ctx).FlowRequest(*app).Execute()
+	if err != nil {
+		return httpToDiag(hr, err)
+	}
+
+	d.SetId(res.Slug)
+	return resourceFlowRead(ctx, d, m)
+}
+
+func resourceFlowRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	c := m.(*APIClient)
+
+	res, hr, err := c.client.FlowsApi.FlowsInstancesRetrieve(ctx, d.Id()).Execute()
+	if err != nil {
+		return httpToDiag(hr, err)
+	}
+
+	d.Set("name", res.Name)
+	d.Set("slug", res.Slug)
+	d.Set("title", res.Title)
+	d.Set("designation", res.Designation)
+	d.Set("policy_engine_mode", res.PolicyEngineMode)
+	d.Set("compatibility_mode", res.CompatibilityMode)
+	return diags
+}
+
+func resourceFlowUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*APIClient)
+
+	app, di := resourceFlowSchemaToModel(d, c)
+	if di != nil {
+		return di
+	}
+
+	res, hr, err := c.client.FlowsApi.FlowsInstancesUpdate(ctx, d.Id()).FlowRequest(*app).Execute()
+	if err != nil {
+		return httpToDiag(hr, err)
+	}
+
+	d.SetId(res.Slug)
+	return resourceFlowRead(ctx, d, m)
+}
+
+func resourceFlowDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*APIClient)
+	hr, err := c.client.FlowsApi.FlowsInstancesDestroy(ctx, d.Id()).Execute()
+	if err != nil {
+		return httpToDiag(hr, err)
+	}
+	return diag.Diagnostics{}
+}
---
+++ internal/provider/resource_flow_test.go
@@ -0,0 +1,42 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccResourceFlow(t *testing.T) {
+	rName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceFlowSimple(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("authentik_flow.flow", "name", rName),
+				),
+			},
+			{
+				Config: testAccResourceFlowSimple(rName + "test"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("authentik_flow.flow", "name", rName+"test"),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceFlowSimple(name string) string {
+	return fmt.Sprintf(`
+resource "authentik_flow" "flow" {
+  name = "%[1]s"
+  title ="%[1]s"
+  slug  ="%[1]s"
+  designation = "authorization"
+}
+`, name)
+}
