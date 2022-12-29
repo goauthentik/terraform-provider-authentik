ebc267b97b858485e9d44e15345292dfcfc00536
Jens Langhammer <jens.langhammer@beryju.org>
2022-12-30 00:12:04 +0100

    add new fields for 2022.12

--- internal/provider/resource_stage_captcha.go
+++ internal/provider/resource_stage_captcha.go
@@ -26,6 +26,14 @@ func resourceStageCaptcha() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+			"js_url": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"api_url": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 			"private_key": {
 				Type:      schema.TypeString,
 				Required:  true,
@@ -40,6 +48,14 @@ func resourceStageCaptchaSchemaToProvider(d *schema.ResourceData) *api.CaptchaSt
 		Name:       d.Get("name").(string),
 		PublicKey:  d.Get("public_key").(string),
 		PrivateKey: d.Get("private_key").(string),
+	}
+	if v, ok := d.GetOk("js_url"); ok {
+		vv := v.(string)
+		r.JsUrl = stringToPointer(vv)
+	}
+	if v, ok := d.GetOk("api_url"); ok {
+		vv := v.(string)
+		r.ApiUrl = stringToPointer(vv)
 	}
 	return &r
 }
@@ -69,6 +85,8 @@ func resourceStageCaptchaRead(ctx context.Context, d *schema.ResourceData, m int
 
 	setWrapper(d, "name", res.Name)
 	setWrapper(d, "public_key", res.PublicKey)
+	setWrapper(d, "api_url", res.GetApiUrl())
+	setWrapper(d, "js_url", res.GetJsUrl())
 	return diags
 }
 
