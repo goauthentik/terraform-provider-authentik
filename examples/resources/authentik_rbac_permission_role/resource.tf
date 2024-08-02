3b5e9fa422bcf84521c9098e1b3bf1521fc7eb89
Jens Langhammer <jens.langhammer@beryju.org>
2024-08-02 16:58:31 +0200

    add RBAC examples

---
+++ examples/resources/authentik_rbac_permission_role/resource.tf
@@ -0,0 +1,31 @@
+# Assign a global permission to a role
+
+resource "authentik_rbac_role" "my-role" {
+  name = "my-role"
+}
+
+resource "authentik_rbac_permission_role" "global-permission" {
+  role       = authentik_rbac_role.my-role.id
+  model      = "authentik_flows.flow"
+  permission = "inspect_flow"
+}
+
+# Assign an object permission to a role
+
+resource "authentik_flow" "flow" {
+  name        = "test-flow"
+  title       = "Test flow"
+  slug        = "test-flow"
+  designation = "authorization"
+}
+
+resource "authentik_rbac_role" "my-role" {
+  name = "my-role"
+}
+
+resource "authentik_rbac_permission_role" "global-permission" {
+  role       = authentik_rbac_role.my-role.id
+  model      = "authentik_flows.flow"
+  permission = "inspect_flow"
+  object_id  = authentik_flow.flow.uuid
+}
