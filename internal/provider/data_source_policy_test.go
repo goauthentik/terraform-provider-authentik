6420e84dd7f57759512da04a9c3a15ce57d6ed8f
Lars With <lars@with.de>
2024-08-12 11:38:19 +0200

    implement datasource policy

---
+++ internal/provider/data_source_policy_test.go
@@ -0,0 +1,28 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDataSourcePolicy(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourcePolicySimple,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.authentik_stage.default-authentication-flow-password-stage", "name", "default-authentication-flow-password-stage"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataSourcePolicySimple = `
+data "authentik_policy" "default-authentication-flow-password-stage" {
+  name = "default-authentication-flow-password-stage"
+}
+`
