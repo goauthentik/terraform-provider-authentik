d161223fcd1c513347087fabb5a553ddcbb3217f
Ryan Pesek <pesekryan@gmail.com>
2024-09-19 23:29:26 -0500

    add tests for include_users

--- internal/provider/data_source_groups_test.go
+++ internal/provider/data_source_groups_test.go
@@ -16,6 +16,23 @@ func TestAccDataSourceGroups(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.authentik_groups.admins", "groups.0.name", "authentik Admins"),
 					resource.TestCheckResourceAttr("data.authentik_groups.admins", "groups.0.is_superuser", "true"),
+					resource.TestCheckResourceAttr("data.authentik_groups.admins", "groups.0.users_obj.#", "1"),
+				),
+			},
+			{
+				Config: testAccDataSourceGroupsSimple,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.authentik_groups.admins_no_users_obj", "groups.0.name", "authentik Admins"),
+					resource.TestCheckResourceAttr("data.authentik_groups.admins_no_users_obj", "groups.0.is_superuser", "true"),
+					resource.TestCheckResourceAttr("data.authentik_groups.admins_no_users_obj", "groups.0.users_obj.#", "0"),
+				),
+			},
+			{
+				Config: testAccDataSourceGroupsSimple,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.authentik_groups.admins_include_users_obj", "groups.0.name", "authentik Admins"),
+					resource.TestCheckResourceAttr("data.authentik_groups.admins_include_users_obj", "groups.0.is_superuser", "true"),
+					resource.TestCheckResourceAttr("data.authentik_groups.admins_include_users_obj", "groups.0.users_obj.#", "1"),
 				),
 			},
 		},
@@ -25,5 +42,15 @@ func TestAccDataSourceGroups(t *testing.T) {
 const testAccDataSourceGroupsSimple = `
 data "authentik_groups" "admins" {
   name = "authentik Admins"
+}
+
+data "authentik_groups" "admins_no_users_obj" {
+  name          = "authentik Admins"
+  include_users = false
+}
+
+data "authentik_groups" "admins_include_users_obj" {
+  name          = "authentik Admins"
+  include_users = true
 }
 `
