cbc532b09cf2f2f4e72b2047d0cd4237be431cf2
Andrey Tatarinov <a@tatarinov.co>
2025-11-25 05:43:01 +0400

    Add `authentik_outpost_provider_attachment` resource and `authentik_outpost` data source (#783)
    * make devcontainer work
    
    * implement data outpost_provider and make tests work
    
    * add doc
    
    * refactor: optimize outpost lookup by name using API's `NameIexact` filter
    
    * docs: Add `authentik_outpost` data source documentation and fix naming in outpost_provider_attachement
    
    * make test UI work in devcontainer mode
    
    * refactor: switch to partial update for outpost provider attachment
    
    * refactor: replace direct state setting with helper function in outpost data source and provider attachment

---
+++ pkg/provider/data_source_outpost.go
@@ -0,0 +1,60 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"goauthentik.io/terraform-provider-authentik/pkg/helpers"
+)
+
+func dataSourceOutpost() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceOutpostRead,
+		Description: "Applications --- Get outposts by id or name",
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ExactlyOneOf: []string{"id", "name"},
+			},
+			"name": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ExactlyOneOf: []string{"id", "name"},
+			},
+		},
+	}
+}
+
+func dataSourceOutpostRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*APIClient)
+
+	if id, ok := d.GetOk("id"); ok {
+		res, hr, err := c.client.OutpostsApi.OutpostsInstancesRetrieve(ctx, id.(string)).Execute()
+		if err != nil {
+			return helpers.HTTPToDiag(d, hr, err)
+		}
+		d.SetId(res.Pk)
+		helpers.SetWrapper(d, "name", res.Name)
+		return nil
+	}
+
+	if name, ok := d.GetOk("name"); ok {
+		res, hr, err := c.client.OutpostsApi.OutpostsInstancesList(ctx).NameIexact(name.(string)).Execute()
+		if err != nil {
+			return helpers.HTTPToDiag(d, hr, err)
+		}
+		if len(res.Results) < 1 {
+			return diag.Errorf("No matching outpost found")
+		}
+		if len(res.Results) > 1 {
+			return diag.Errorf("Multiple outposts found")
+		}
+		d.SetId(res.Results[0].Pk)
+		helpers.SetWrapper(d, "name", res.Results[0].Name)
+		return nil
+	}
+
+	return diag.Errorf("Neither id nor name were provided")
+}
---
+++ pkg/provider/resource_outpost_provider_attachment.go
@@ -0,0 +1,148 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	api "goauthentik.io/api/v3"
+	"goauthentik.io/terraform-provider-authentik/pkg/helpers"
+)
+
+func resourceOutpostProviderAttachment() *schema.Resource {
+	return &schema.Resource{
+		Description:   "Applications --- ",
+		CreateContext: resourceOutpostProviderAttachmentCreate,
+		ReadContext:   resourceOutpostProviderAttachmentRead,
+		DeleteContext: resourceOutpostProviderAttachmentDelete,
+		UpdateContext: nil,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"outpost": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"protocol_provider": {
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceOutpostProviderAttachmentCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*APIClient)
+
+	outpostID := d.Get("outpost").(string)
+	providerID := int32(d.Get("protocol_provider").(int))
+
+	// Get current outpost
+	outpost, hr, err := c.client.OutpostsApi.OutpostsInstancesRetrieve(ctx, outpostID).Execute()
+	if err != nil {
+		return helpers.HTTPToDiag(d, hr, err)
+	}
+
+	// Check if provider is already attached
+	for _, p := range outpost.Providers {
+		if p == providerID {
+			// Already attached, just set ID
+			d.SetId(fmt.Sprintf("%s:%d", outpostID, providerID))
+			return resourceOutpostProviderAttachmentRead(ctx, d, m)
+		}
+	}
+
+	// Add provider
+	outpost.Providers = append(outpost.Providers, providerID)
+
+	// Update outpost
+	req := api.PatchedOutpostRequest{
+		Providers: outpost.Providers,
+	}
+
+	_, hr, err = c.client.OutpostsApi.OutpostsInstancesPartialUpdate(ctx, outpostID).PatchedOutpostRequest(req).Execute()
+	if err != nil {
+		return helpers.HTTPToDiag(d, hr, err)
+	}
+
+	d.SetId(fmt.Sprintf("%s:%d", outpostID, providerID))
+	return resourceOutpostProviderAttachmentRead(ctx, d, m)
+}
+
+func resourceOutpostProviderAttachmentRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*APIClient)
+
+	// Parse ID
+	parts := strings.Split(d.Id(), ":")
+	if len(parts) != 2 {
+		return diag.Errorf("Invalid ID format")
+	}
+	outpostID := parts[0]
+	// providerID is parts[1] but we need to check if it exists in the outpost
+
+	outpost, hr, err := c.client.OutpostsApi.OutpostsInstancesRetrieve(ctx, outpostID).Execute()
+	if err != nil {
+		return helpers.HTTPToDiag(d, hr, err)
+	}
+
+	providerIDInt, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	found := false
+	for _, p := range outpost.Providers {
+		if p == int32(providerIDInt) {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		d.SetId("")
+		return nil
+	}
+
+	helpers.SetWrapper(d, "outpost", outpostID)
+	helpers.SetWrapper(d, "protocol_provider", int(providerIDInt))
+
+	return nil
+}
+
+func resourceOutpostProviderAttachmentDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*APIClient)
+
+	outpostID := d.Get("outpost").(string)
+	providerID := int32(d.Get("protocol_provider").(int))
+
+	// Get current outpost
+	outpost, hr, err := c.client.OutpostsApi.OutpostsInstancesRetrieve(ctx, outpostID).Execute()
+	if err != nil {
+		return helpers.HTTPToDiag(d, hr, err)
+	}
+
+	// Remove provider
+	newProviders := []int32{}
+	for _, p := range outpost.Providers {
+		if p != providerID {
+			newProviders = append(newProviders, p)
+		}
+	}
+
+	// Update outpost
+	req := api.PatchedOutpostRequest{
+		Providers: newProviders,
+	}
+
+	_, hr, err = c.client.OutpostsApi.OutpostsInstancesPartialUpdate(ctx, outpostID).PatchedOutpostRequest(req).Execute()
+	if err != nil {
+		return helpers.HTTPToDiag(d, hr, err)
+	}
+
+	return nil
+}
